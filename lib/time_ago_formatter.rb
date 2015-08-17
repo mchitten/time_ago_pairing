6f114c9a90483f76d4dcff123fdcdf145678477b
Michael Chittenden <mchitten@gmail.com>
2015-08-17 13:00:27 -0400

    Pass 29 seconds

--- lib/time_ago_formatter.rb
+++ lib/time_ago_formatter.rb
@@ -1,4 +1,26 @@
 require 'active_support'
+require 'active_support/core_ext/object'
 
 class TimeAgoFormatter
+  def initialize(time)
+    @now = Time.now
+    @time = time
+  end
+
+  def to_human
+    seconds = 1
+    minutes = 60
+    hours = 1.hour
+    day = 1.day.to_i
+    month = 1.month
+    year = 1.year
+
+    if @now - @time < 30.seconds
+      @human = 'in the last minute'
+    else
+      @human = 'test'
+    end
+
+    @human
+  end
 end
--- spec/unit/time_ago_formatter_spec.rb
+++ spec/unit/time_ago_formatter_spec.rb
@@ -12,4 +12,53 @@ RSpec.describe TimeAgoFormatter do
 
     expect(described_class.new(ago).to_human).to eq human_version
   end
+
+  context '29 seconds ago' do
+    let(:ago) { 29.seconds.ago }
+    it 'returns "in the last minute"' do
+      expect(described_class.new(ago).to_human).to eq "in the last minute"
+    end
+  end
+
+  context '30 seconds ago' do
+    let(:ago) { 1.minute.ago }
+    it 'returns "1 minute ago"' do
+      expect(described_class.new(ago).to_human).to eq "1 minute ago"
+    end
+  end
+
+  context '2 minutes 0 seconds ago' do
+    let(:ago) { 2.minutes.ago }
+    it 'returns "2 minutes ago"' do
+      expect(described_class.new(ago).to_human).to eq "2 minutes ago"
+    end
+  end
+
+  context '2 minutes 31 seconds ago' do
+    let(:ago) { 3.minutes.ago }
+    it 'returns "3 minutes ago"' do
+      expect(described_class.new(ago).to_human).to eq "3 minutes ago"
+    end
+  end
+
+  context '1 hour 3 minutes ago' do
+    let(:ago) { (1.hour + 3.minutes).ago }
+    it 'returns 1 hour, 3 minutes ago' do
+      expect(described_class.new(ago).to_human).to eq '1 hour, 3 minutes ago'
+    end
+  end
+
+  context '3 days, 1 hour and 5 minutes ago' do
+    let(:ago) { (3.days + 1.hour + 5.minutes).ago }
+    it 'returns "3 days, 1 hour and 5 minutes ago"' do
+      expect(described_class.new(ago).to_human).to eq "3 days, 1 hour and 5 minutes ago"
+    end
+  end
+
+  context '1 month, 2 days and 3 hours ago' do
+    let(:ago) { (1.month + 2.days + 3.hours).ago }
+    it 'returns "1 month, 2 days, 3 hours ago"' do
+      expect(described_class.new(ago).to_human).to eq "1 month, 2 days, 3 hours ago"
+    end
+  end
 end
